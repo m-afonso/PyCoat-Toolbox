054fe9b1388503dbfedba1cc2374dd8ce8c5f2cd
m-afonso <64047531+m-afonso@users.noreply.github.com>
2024-02-27 23:04:19 -0300

    Software name change

--- src/main_interface.py
+++ src/main_interface.py
@@ -4,7 +4,7 @@ from src.interface_nv import *
 
 
 def main_interface():
-    program_title = 'Coatings Calculator'
+    program_title = 'PyCoat Wizard'
     version = 'v0.0.1'
     release_date = '2024'
 
