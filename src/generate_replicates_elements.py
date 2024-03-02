7cf36450dbe8db2288a173e85322fbc7f0219441
m-afonso <64047531+m-afonso@users.noreply.github.com>
2024-03-02 18:14:31 -0300

    Code improvements
    Readability improvements
    
    Code improvements
    
    Code improvements
    
    Code improvements
    
    Code improvements
    
    Code improvements
    
    Code improvements
    
    Code improvements

---
+++ src/generate_replicates_elements.py
@@ -0,0 +1,137 @@
+import tkinter as tk
+import numpy as np
+from tkinter import messagebox
+from typing import List, Dict, Tuple
+
+
+def generate_replicates_elements(
+        interface_window,
+        _replicates_number: int = 5,
+        _initial_row: int = 0,
+        _initial_column: int = 0,
+        _head_titles: Tuple[str, str, str, str] = ('Empty', 'Sample', 'Final', 'Result')):
+
+    _replicates_list = list()
+
+    # head labels
+    replicate_label = tk.Label(interface_window, text='Replicate ID')
+    replicate_label.grid(row=_initial_row, column=0, padx=25)
+
+    empty_weight_label = tk.Label(interface_window, text=_head_titles[0])
+    empty_weight_label.grid(row=_initial_row, column=1, padx=25)
+
+    sample_weight_label = tk.Label(interface_window, text=_head_titles[1])
+    sample_weight_label.grid(row=_initial_row, column=2, padx=25)
+
+    final_weight_label = tk.Label(interface_window, text=_head_titles[2])
+    final_weight_label.grid(row=_initial_row, column=3, padx=25)
+
+    result_label = tk.Label(interface_window, text=_head_titles[3])
+    result_label.grid(row=_initial_row, column=4, padx=25)
+
+    for replicate in range(_replicates_number):
+        replicate_dict = {
+            'id': tk.Label(interface_window, text=str(replicate + 1)),
+            'zero_entry': tk.Entry(interface_window, justify='center'),
+            'sample_entry': tk.Entry(interface_window, justify='center'),
+            'final_entry': tk.Entry(interface_window, justify='center'),
+            'result': tk.Entry(interface_window, state='readonly', justify='center')
+        }
+
+        _replicates_list.append(replicate_dict)
+
+    padx_value = 25
+
+    for replicate_number in range(len(_replicates_list)):
+        _replicates_list[replicate_number]['id'].grid(
+            column=_initial_column,
+            row=_initial_row + replicate_number + 1,
+            padx=padx_value
+        )
+
+        _replicates_list[replicate_number]['zero_entry'].grid(
+            column=_initial_column + 1,
+            row=_initial_row + replicate_number + 1,
+            padx=padx_value
+        )
+
+        _replicates_list[replicate_number]['sample_entry'].grid(
+            column=_initial_column + 2,
+            row=_initial_row + replicate_number + 1,
+            padx=padx_value)
+
+        _replicates_list[replicate_number]['final_entry'].grid(
+            column=_initial_column + 3,
+            row=_initial_row + replicate_number + 1,
+            padx=padx_value
+        )
+
+        _replicates_list[replicate_number]['result'].grid(
+            column=_initial_column + 4,
+            row=_initial_row + replicate_number + 1,
+            padx=padx_value
+        )
+
+    _mean_entry = tk.Entry(
+        interface_window,
+        state='readonly',
+        justify='center')
+
+    _mean_entry.grid(row=_initial_row + len(_replicates_list) + 1, column=_initial_column + 4, padx=padx_value)
+
+    return _replicates_list, _mean_entry
+
+
+def get_values_replicates_elements(replicates_elements_list: List[Dict[str, tk.Entry | tk.Label]],
+                                   result_entry: tk.Entry,
+                                   result_function) -> None:
+
+    results_list = list()
+
+    for replicate in replicates_elements_list:
+
+        def element_input_checker(_replicate: dict[str, tk.Entry | tk.Label]) -> bool:
+            for element in _replicate.values():
+                if isinstance(element, tk.Entry) and element["state"] == 'normal':
+                    if element.get() == "":
+                        return False
+                    else:
+                        try:
+                            if float(element.get()) <= 0:
+                                messagebox.showerror('Error Message',
+                                                     'All values must be greater than zero.')
+                                return False
+                        except ValueError:
+                            messagebox.showerror('Error Message',
+                                                 'All filled entries must be numeric and greater than zero.')
+                            return False
+            return True
+
+        if not element_input_checker(replicate):
+            continue
+
+        empty_value = float(replicate['zero_entry'].get())
+        sample_value = float(replicate['sample_entry'].get())
+        final_value = float(replicate['final_entry'].get())
+
+        replicate_result = result_function(empty_value, sample_value, final_value)
+        results_list.append(replicate_result)
+        replicate['result'].config(state='normal')
+        replicate['result'].delete(0, 'end')
+        replicate['result'].insert(0, f'{(replicate_result*100):.2f}')
+        replicate['result'].config(state='readonly')
+
+    if results_list:
+        results_mean = np.mean(results_list)
+        results_std = np.std(results_list)
+    else:
+        return
+
+    result_entry.config(state='normal')
+    result_entry.delete(0, 'end')
+    result_entry.insert(0, f'{(results_mean * 100):.2f} ± {(results_std * 100):.2f}')
+    result_entry.config(state='readonly')
+
+
+if __name__ == '__main__':
+    pass
--- src/interface_module_nv.py
+++ src/interface_module_nv.py
@@ -1,6 +1,8 @@
 import tkinter as tk
-from tkinter import ttk
 import numpy as np
+from tkinter import messagebox
+from typing import List, Dict, Tuple
+from src.generate_replicates_elements import *
 
 
 def nv_interface(master_window=None):
@@ -14,58 +16,14 @@ def nv_interface(master_window=None):
 
     initial_row = 1
     initial_column = 0
-
-    def generate_replicates(replicates_number):
-        nonlocal initial_row, initial_column
-
-        # top labels
-        replicate_label = tk.Label(nv_interface_window, text='Replicate ID')
-        replicate_label.grid(row=initial_row, column=0, padx=25)
-        zero_weight_label = tk.Label(nv_interface_window, text='Empty Cup (M0)')
-        zero_weight_label.grid(row=initial_row, column=1, padx=25)
-        sample_weight_label = tk.Label(nv_interface_window, text='Sample (M1)')
-        sample_weight_label.grid(row=initial_row, column=2, padx=25)
-        final_weight_label = tk.Label(nv_interface_window, text='Residue + Cup (M2)')
-        final_weight_label.grid(row=initial_row, column=3, padx=25)
-        nv_label = tk.Label(nv_interface_window, text='NV%')
-        nv_label.grid(row=initial_row, column=4, padx=25)
-
-        replicates_list = list()
-
-        for replicate in range(replicates_number):
-
-            replicate_dict = {
-                'id': tk.Label(nv_interface_window, text=str(replicate + 1)),
-                'zero_entry': tk.Entry(nv_interface_window, justify='center'),
-                'sample_entry': tk.Entry(nv_interface_window, justify='center'),
-                'final_entry': tk.Entry(nv_interface_window, justify='center'),
-                'result': tk.Entry(nv_interface_window, state='readonly', justify='center')
-            }
-
-            replicates_list.append(replicate_dict)
-
-        padx_value = 25
-        for replicate_number in range(len(replicates_list)):
-            replicates_list[replicate_number]['id'].grid(row=initial_row + replicate_number + 1, column=initial_column,
-                                                         padx=padx_value)
-            replicates_list[replicate_number]['zero_entry'].grid(row=initial_row + replicate_number + 1,
-                                                                 column=initial_column + 1, padx=padx_value)
-            replicates_list[replicate_number]['sample_entry'].grid(row=initial_row + replicate_number + 1,
-                                                                   column=initial_column + 2, padx=padx_value)
-            replicates_list[replicate_number]['final_entry'].grid(row=initial_row + replicate_number + 1,
-                                                                  column=initial_column + 3, padx=padx_value)
-            replicates_list[replicate_number]['result'].grid(row=initial_row + replicate_number + 1,
-                                                             column=initial_column + 4, padx=padx_value)
-
-        # mean_label = tk.Label(nv_interface_window, text='Average')
-        # mean_label.grid(row=initial_row + len(replicates_list), column=initial_column)
-        mean_entry = tk.Entry(nv_interface_window, state='readonly', justify='center')
-        mean_entry.grid(row=initial_row + len(replicates_list) + 1, column=initial_column + 4, padx=padx_value)
-
-        return replicates_list, mean_entry
-
     replicates_number = 5
-    replicates_list, mean_entry = generate_replicates(replicates_number)
+    replicates_list, mean_entry = generate_replicates_elements(
+        nv_interface_window,
+        _replicates_number=replicates_number,
+        _initial_row=initial_row,
+        _initial_column=initial_column,
+        _head_titles=('Empty Cup (M0)', 'Sample (M1)', 'Residue + Cup (M2)', '%NV')
+    )
 
     # procedure widget
     procedure_label = tk.Label(nv_interface_window, justify='left',
@@ -85,43 +43,41 @@ def nv_interface(master_window=None):
 
     procedure_label.grid(row=initial_row + replicates_number + 6, column=0, pady=10, columnspan=5, sticky="W")
 
-    run_button = tk.Button(nv_interface_window, text="Run", command=lambda: get_values(), width=15)
+    run_button = tk.Button(
+        nv_interface_window,
+        text="Run",
+        command=lambda: get_values_replicates_elements(replicates_list,
+                                                       mean_entry,
+                                                       nv_test),
+        width=15
+    )
+
     run_button.grid(row=initial_row + replicates_number + 8, column=0, pady=25, padx=10)
-
-    def get_values():
-        nv_results = list()
-        nonlocal replicates_list
-        nonlocal initial_column, initial_row, mean_entry
-
-        for replicate in replicates_list:
-
-            zero_value = float(replicate['zero_entry'].get()) if str.isnumeric(replicate['zero_entry'].get().replace(".", "")) else 0.0
-            sample_value = float(replicate['sample_entry'].get()) if str.isnumeric(replicate['sample_entry'].get().replace(".", "")) else 0.0
-            final_value = float(replicate['final_entry'].get()) if str.isnumeric(replicate['final_entry'].get().replace(".", "")) else 0.0
-
-            if sample_value == 0 or zero_value == 0 or final_value == 0:
-                continue
-
-            replicate_result = (final_value - zero_value) / sample_value
-            nv_results.append(replicate_result)
-            replicate['result'].config(state='normal')
-            replicate['result'].delete(0, 'end')
-            replicate['result'].insert(0, f'{(replicate_result*100):.2f}')
-            replicate['result'].config(state='readonly')
-
-        if nv_results:
-            nv_results_mean = np.mean(nv_results)
-            nv_results_std = np.std(nv_results)
-        else:
-            return
-
-        mean_entry.config(state='normal')
-        mean_entry.delete(0, 'end')
-        mean_entry.insert(0, f'{(nv_results_mean * 100):.2f} ± {(nv_results_std * 100):.2f}')
-        mean_entry.config(state='readonly')
 
     if master_window is None:
         nv_interface_window.mainloop()
+
+
+def nv_test(empty: float | int,
+            sample: float | int,
+            final: float | int) -> float | None:
+
+    if not isinstance(empty, (float, int)):
+        raise TypeError('Empty weight must be a decimal or integer number.')
+
+    if not isinstance(sample, (float, int)):
+        raise TypeError('Final weight must be a decimal or integer number.')
+
+    if not isinstance(sample, (float, int)):
+        raise TypeError('Sample weight must be a decimal or integer number.')
+
+    if final < empty:
+        raise ValueError("\n\nFinal weight must be greater or equal to empty weight.")
+
+    if sample <= 0:
+        raise ValueError("\n\nSample weight must be greater than zero.")
+
+    return (final-empty)/sample
 
 
 if __name__ == '__main__':
